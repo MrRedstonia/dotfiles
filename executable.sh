4e28931de28a997f8364c7b5c9d58387dfb22b3e
MrRedstonia <62774045+MrRedstonia@users.noreply.github.com>
2026-01-17 05:11:13 +0100

    Create script to update file permissions
    This script updates the permissions of specified files to make them executable, while providing feedback on the process.

---
+++ executable.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+{
+    FILES=(
+        "$HOME/.config/hypr/scripts/screenshot/region.sh"
+        "$HOME/.config/hypr/scripts/screenshot/monitor.sh"
+        "$HOME/.config/hypr/scripts/screenshot/window.sh"
+        "$HOME/.config/hypr/scripts/screenshot/fullscreen"
+        "$HOME/.config/hypr/scripts/close.sh"
+        "$HOME/.config/hypr/scripts/power_menu.sh"
+        "$HOME/.config/hypr/scripts/power_restore.sh"
+        "$HOME/.config/hypr/scripts/power_save.sh"
+        "$HOME/.config/wofi/emoji-picker.sh"
+        "$HOME/.config/waybar/scripts/volume-toggle.sh"
+    )
+
+    echo "------------------------------------------"
+    echo "🚀 Starting File Permissions Update"
+    echo "------------------------------------------"
+
+    for FILE in "${FILES[@]}"; do
+        # Check if the file exists
+        if [ -f "$FILE" ]; then
+            chmod +x "$FILE"
+            echo "✅ SUCCESS: Made executable -> $FILE"
+        else
+            echo "❌ ERROR: File not found    -> $FILE"
+        fi
+    done
+
+    echo "------------------------------------------"
+    echo "🎉 All tasks complete!"
+}
